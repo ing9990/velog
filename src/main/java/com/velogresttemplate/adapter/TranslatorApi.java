5d072d5b55fe502918eaecf171b9606b385536f9
ing9990 <gimgau0218@naver.com>
2023-05-08 11:42:19 +0900

    style: Unused 제거

--- src/main/java/com/velogresttemplate/adapter/TranslatorApi.java
+++ src/main/java/com/velogresttemplate/adapter/TranslatorApi.java
@@ -6,15 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import com.velogresttemplate.port.TranslatorPort;
 
-import lombok.extern.slf4j.Slf4j;
-
 @RestController
 @RequestMapping("/api/v1")
-@Slf4j
 public class TranslatorApi {
 
 	private final TranslatorPort translatorPort;
@@ -23,27 +19,13 @@ public class TranslatorApi {
 		this.translatorPort = translatorPort;
 	}
 
-	@GetMapping("/velog")
-	public ResponseEntity<?> velog() {
-		RestTemplate restTemplate = new RestTemplate();
-
-		ResponseEntity<Object> response = restTemplate.getForEntity("http://localhost:7777/api/v1/test",
-			Object.class);
-
-		System.out.println(response);
-
-		return ResponseEntity.status(HttpStatus.OK).body(response);
-	}
-
 	@GetMapping("/translate")
 	public ResponseEntity<?> translate(
 		@RequestParam(name = "text") String text,
 		@RequestParam(name = "source", required = false, defaultValue = "ko") String source,
 		@RequestParam(name = "target", required = false, defaultValue = "en") String target
 	) {
-		var response = translatorPort.translate(source, target, text);
-
-		return ResponseEntity.status(HttpStatus.OK).body(response);
+		return ResponseEntity.status(HttpStatus.OK)
+			.body(translatorPort.translate(source, target, text));
 	}
-
 }
